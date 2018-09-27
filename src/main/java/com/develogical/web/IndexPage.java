d4aaf3dea818e64afb1ab4fbdb7281fd5d83ee38
divya surendran <divya.spec@gmail.com>
2018-09-27 11:58:02 +0100

    1st Commit

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -9,7 +9,7 @@ public class IndexPage extends HtmlPage {
         writer.println(
                 "<h1>Welcome!</h1>" +
                 "<p>Enter your query in the box below: " +
-                  "<form><input type=\"text\" name=\"q\" />" +
+                  "<form><input type=\"text\" AwesomeName=\"q\" />" +
                     "<input type=\"submit\">" +
                   "</form>" +
                 "</p>");
