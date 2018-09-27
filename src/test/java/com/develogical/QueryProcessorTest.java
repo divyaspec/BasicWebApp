8d012b9db0499de2d423bb08a21c93923fba5f4a
divya surendran <divya.spec@gmail.com>
2018-09-27 14:19:51 +0100

    web server request

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -24,4 +24,11 @@ public class QueryProcessorTest {
     public void isNotCaseSensitive() throws Exception {
         assertThat(queryProcessor.process("shakespeare"), containsString("playwright"));
     }
+
+    @Test
+    public void isAskingMyNameThenReturnName() throws Exception {
+        assertThat(queryProcessor.process("what is your name"), containsString("awesome"));
+    }
+
+
 }
