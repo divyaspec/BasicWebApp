03d29a1f694bbd64caa4099e366f609f8f5dd3f2
divya surendran <divya.spec@gmail.com>
2018-09-27 14:43:51 +0100

    plus

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -9,7 +9,13 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if(query.toLowerCase().contains("what is your name")) {
             return "awesome";
+        }  else if(query.toLowerCase().contains("what is 2 plus 13 ")) {
+            return "15";
         }
+        else if(query.toLowerCase().contains("which of the following numbers is the largest: 375, 99")) {
+            return "375";
+        }
+        // which of the following numbers is the largest: 375, 99
         return "";
     }
 }
